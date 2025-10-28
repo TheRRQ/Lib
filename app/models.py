e13e6dc66e8187b4707b53051b3c98ceef203dd2
TheRRQ <theruraq5@gmail.com>
2025-10-28 18:05:21 +0100

    model to DB

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+class Book(Base):
+
+    __tablename__ = "books"
+
+
+    id = Column(Integer, primary_key= True, index=True)
+    serial_number = Column(Integer, unique=True, nullable=False)
+
+    title = Column(String(100), nullable = False)
+    author = Column(String(100), nullable=False)
+
+    is_borrowed = Column(Boolean, default=False)
+    borrowed_by = Column(Integer, nullable= True)
+    borrowed_at = Column(DateTime, nullable= True)
+
