4cc6e10fedf56931f39d86f484ad03a6a8e72200
TheRRQ <theruraq5@gmail.com>
2025-10-28 20:45:55 +0100

    constraints for number, forbid sn update, borrowed_by

--- app/schemas.py
+++ app/schemas.py
@@ -1,21 +1,22 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import Optional
 
 
 
 class BookBase(BaseModel):
-    serial_number : int
+    serial_number : int = Field(... , ge=100000, le=999999, description="6 digit book SN")
     title : str
     author : str
 
 class BookCreate(BookBase):
     pass
 
-class BookUpdate(BookBase):
-    
-    is_borrowed : bool
-    borrower_id : Optional[int] = None
+class BookUpdate(BaseModel):
+    title : Optional[str] = None
+    author : Optional[str] = None
+    is_borrowed : Optional[bool] = None
+    borrowed_by : Optional[int] = None
     borrowed_at : Optional[datetime] = None
 
 
@@ -24,18 +25,10 @@ class Book(BookBase):
     id : int
 
     is_borrowed : bool
-    borrower_id : Optional[int] = None
+    borrowed_by : Optional[int] = None
     borrowed_at : Optional[datetime] = None
 
 
 
     class Config:
-        orm_mode = True
-
-
-
-
-
-
-
-
+        from_attributes = True
