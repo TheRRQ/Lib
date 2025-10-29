193ccacc4bdd26495416e8a3c5df437b8b46d3ba
TheRRQ <theruraq5@gmail.com>
2025-10-29 12:46:08 +0100

    waiting for postgres function

--- app/main.py
+++ app/main.py
@@ -1,14 +1,42 @@
 from fastapi import FastAPI
 from sqlalchemy import text
-
+import time
 from app.database import engine
-
+from sqlalchemy.exc import OperationalError
 from app.routes import router
 from app.models import Base
+import logging
+import sys
 
 
 
 app = FastAPI(title="Library app")
+
+
+logger = logging.getLogger("uvicorn.error")
+
+
+def wait_for_db(retries = 3, delay = 3):
+    """
+    Ensuring that this service doesnt crash if posgress starts first, and is not ready for connections.
+    """
+    for attempt in range(1, retries+1):
+        try: 
+            with engine.connect() as conn:
+                conn.execute(text("SELECT 1"))
+            logger.info("Connection to DB established.")
+            return
+        
+        except OperationalError as exc:
+            logger.warning(f"Attempt {attempt} / {retries} failed, wating {delay} sec")
+            if attempt == retries:
+                logger.error("Failed to connect to DB, closing application.")
+                sys.exit(1)
+            
+            time.sleep(delay)
+
+
+wait_for_db(retries=3, delay=3)
 
 Base.metadata.create_all(bind = engine)
 
